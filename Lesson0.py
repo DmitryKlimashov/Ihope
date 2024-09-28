10153a1d2776ad68efb89258eb3f775e44f6e11f
DmitryKlimashov <klimashov758@gmail.com>
2024-09-28 10:43:37 +0300

    Add files via upload

---
+++ Lesson0.py
@@ -0,0 +1,15 @@
+#"1st program"
+print(9 ** 0.5 * 5)
+#"2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+#"3rd program"
+print(2 * 2 + 2 == (2 + 2) * 2) # я точно не знаю, что вы в итоге имели под словом приоритет, но я просто сделал задачку что бы появилось false
+#"4th program"
+print( 4 )
+print(float('123.456'))
+print(float(123.456 * 10))
+print(float(123.456 * 100))
+print(float(123.456 * 1000))
+print(int(123456.0))
+print(int(123456.0 % 10))
+print(int(float('123.456') * 10) % 10)
