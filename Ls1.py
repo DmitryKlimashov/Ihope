97ff855a07c55d9945c29fedbbd952b970f44170
DmitryKlimashov <klimashov758@gmail.com>
2024-09-29 18:33:18 +0300

    Add files via upload

---
+++ Ls1.py
@@ -0,0 +1,15 @@
+name = "12"
+name2 ='1.5'
+name3 =("Python")
+print(name)
+print(name2)
+print(name3)
+a = 'Курс:Python, всего задач:12,'
+
+b = "затрачено часов: 1.5,"
+
+c = 'среднее время выполнения 0.125 часа.'
+
+
+
+print(a,b,c)
