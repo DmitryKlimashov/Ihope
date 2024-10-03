795dd649d5bf90bccebffb886584a19ce0ccb706
DmitryKlimashov <klimashov758@gmail.com>
2024-10-03 11:59:53 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,22 @@
+#my_dict = {"Dima": 1995, "Valeriya": 1999, 'Natali': 2002, "Alice": 2001}
+#print(my_dict)
+#my_dict["Dima"] = 1997
+#my_dict["Karina"] = 1998
+#print(my_dict)
+#my_dict.update({"Anton": 1994, "Max": 1991})
+#print(my_dict)
+#del my_dict["Natali"]
+#print(my_dict)
+#print(my_dict.get("Alice"))
+
+set_ = {1, 2, 3, 3, 4, 4, 4, 5, 5, "Mix" , (1, 2, 3)}
+print(set_)
+list_ = [1, 2, 5]
+list_ = set(list_)
+print(set(list_))
+print(list_.remove(5))
+print(list_)
+print(list_.add(9))
+print(list_.add("jam")) # Я пишу тут лишнего потому что не свовсем иногда понимаю задачи
+print(list_.add(6.6))
+print(list_)
